84ab3dc5481c231037ebf504ec3aa81599c9bd96
kingvictus <kingvictus@github.com>
2020-08-19 18:46:43 +0400

    get all cars from db endpoint

--- cars/routes/users.js
+++ cars/routes/users.js
@@ -1,6 +1,9 @@
 import express from 'express'
+import cars from '../controllers/cars'
 
 /* GET users listing. */
 const router = express.Router();
+
+router.get('/cars', cars.getAllCars)
 
 module.exports = router;
