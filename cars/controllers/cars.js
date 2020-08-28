276f7634c6807a3973a6896a9289419efdbc9d8b
kingvictus <kingvictus@github.com>
2020-08-28 20:49:46 +0400

    delete car from database endpoint

--- cars/controllers/cars.js
+++ cars/controllers/cars.js
@@ -1,6 +1,6 @@
 import models from '../models/index'
 
-const carsModel = models.cars
+const carModel = models.cars
 
 class cars {
   static welcome(req, res) {
@@ -9,29 +9,28 @@ class cars {
 
   static getAllCars(req, res) {
     // eslint-disable-next-line no-shadow
-    carsModel.findAll().then((cars) => {
+    carModel.findAll().then((cars) => {
       res.status(200).send({ message: 'Cars fetched successfully', cars })
     })
   }
 
   static addCar(req, res) {
-    carsModel
+    carModel
       .create({
         make: req.body.make,
         colour: req.body.colour,
         year: req.body.year,
         price: req.body.price
       })
-      .then((newCar) => res
-        .status(201)
-        .send({ message: 'Car added successfully', newCar }))
+      .then((newCar) =>
+        res.status(201).send({ message: 'Car added successfully', newCar }))
   }
 
   static updateCar(req, res) {
     // eslint-disable-next-line radix
     const id = parseInt(req.params.id)
     // eslint-disable-next-line no-shadow
-    carsModel.findByPk(id).then((cars) => {
+    carModel.findByPk(id).then((cars) => {
       cars
         .update({
           make: req.body.make || cars.make,
@@ -44,6 +43,21 @@ class cars {
             .status(201)
             .send({ message: 'Car added successfully', updatedcar })
         })
+    })
+  }
+
+  static deleteCar(req, res) {
+    // eslint-disable-next-line radix
+    const id = parseInt(req.params.id)
+    carModel.findByPk(id).then((cars) => {
+      if (!cars) {
+        return res.status(404).send({
+          message: 'car not found'
+        })
+      }
+      return cars.destroy().then(() => res.status(204).send({
+        message: 'car deleted successfully'
+      }))
     })
   }
 }
