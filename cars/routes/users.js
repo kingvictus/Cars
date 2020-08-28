bb567f0d67eb58306499fb8c47ca9bb7d1122e2e
kingvictus <kingvictus@github.com>
2020-08-28 21:26:30 +0400

    search car by make from database endpoint

--- cars/routes/users.js
+++ cars/routes/users.js
@@ -9,5 +9,6 @@ router.post('/cars', cars.addCar)
 router.put('/cars/:id', cars.updateCar)
 router.delete('/cars/:id', cars.deleteCar)
 router.get('/cars/:id', cars.getCar)
+router.get('/cars/make', cars.searchCarByMake)
 
 module.exports = router;
