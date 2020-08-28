46ac86de95e612f6abf5fffdbe6401f8b52050f1
kingvictus <kingvictus@github.com>
2020-08-28 20:38:38 +0400

    update car in database endpoint

--- cars/routes/users.js
+++ cars/routes/users.js
@@ -5,6 +5,7 @@ import cars from '../controllers/cars'
 const router = express.Router();
 
 router.get('/cars', cars.getAllCars)
-router.post('/cars', cars.addCars)
+router.post('/cars', cars.addCar)
+router.put('/cars/:id', cars.updateCar)
 
 module.exports = router;
