42255384c6b14bb2823162995c1e80d8466cd7e4
kingvictus <kingvictus@github.com>
2020-08-30 12:23:49 +0400

    setup mocha and chai for unit testing

---
+++ cars/test/cars.test.js
@@ -0,0 +1,23 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+import { expect } from 'chai'
+import supertest from 'supertest'
+import server from '../app'
+
+const chai = require('chai')
+
+const should = chai.should()
+
+const request = supertest.agent(server)
+
+describe('Cars Api', () => {
+  describe('Index route', () => {
+    it('should return welcome message when / route is matched', (done) => {
+      request.get('/api/v1').end((err, res) => {
+        res.status.should.be.equal(200)
+        expect(res.body.message).be.equal('Welcome to Cars Api')
+        done()
+      })
+    })
+  })
+})
